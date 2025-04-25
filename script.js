4e55808ecab3bfae3904a94f6797630c027ecd25
minjongkim1 <minjongkim.production@gmail.com>
2025-04-25 20:11:58 +0200

    mobile

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ setInterval(() => {
   current = (current + 1) % images.length;
   images[current].classList.add('active');
 }, 5000);
+
+const images = document.querySelectorAll('.bg-image');
+
+function updateImagesForScreen() {
+  const isMobile = window.innerWidth <= 600;
+
+  images.forEach((img, index) => {
+    if (isMobile) {
+      img.src = `image${index + 1}-mobile.svg`;
+    } else {
+      img.src = `image${index + 1}.svg`;
+    }
+  });
+}
+
+// 처음 로딩할 때 한번 체크
+updateImagesForScreen();
+
+// 화면 크기 바뀔 때도 체크
+window.addEventListener('resize', updateImagesForScreen);
