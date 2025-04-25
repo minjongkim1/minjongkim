d255242a5c899a81edac458c1571936b17cc543d
minjongkim1 <130797676+minjongkim1@users.noreply.github.com>
2025-04-25 17:51:56 +0200

    Delete darkmode.js

--- darkmode.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.getElementById('darkmode-toggle');
-
-  toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    document.querySelector('.bio').classList.toggle('dark-mode');
-  });
-});
