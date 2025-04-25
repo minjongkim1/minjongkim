5c59c5cc3e942beca0bb5e017045054f04841d6d
minjongkim1 <130797676+minjongkim1@users.noreply.github.com>
2025-04-25 17:52:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const images = document.querySelectorAll('.bg-image');
-let current = 0;
-
-setInterval(() => {
-  images[current].classList.remove('active');
-  current = (current + 1) % images.length;
-  images[current].classList.add('active');
-}, 5000);
